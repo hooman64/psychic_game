d1bb731dd950aa621a8459e49b7c680802c8617d
Hooman <hooman64@users.noreply.github.com>
2018-06-05 07:17:39 -0700

    Psychic Game Final Push

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+// For Tracking Purposes
+var guesses = [];
+var wins = 0;
+var losses = 0;
+var guesscount = 9;
+
+// For Computer Random Letter Assignment (wouldn't change on every run - moved to function)
+var randlet = alphabet[Math.floor(Math.random()*alphabet.length)];
+
+// For Keypress Guesses
+document.onkeypress = function(event) {
+    // For Computer Random Letter Assignment
+    var tries = event.key;
+
+    if(tries == randlet){
+        wins +=1;
+        document.getElementById('wins').innerHTML = wins;
+        guesscount = 9;
+        guesses = [];
+        randlet = alphabet[Math.floor(Math.random()*alphabet.length)];
+    }else{
+        guesscount -=1;
+    }
+
+    if(guesscount === 0){
+        losses +=1;
+        document.getElementById('losses').innerHTML = losses;
+        guesscount = 9;
+        guesses = [];
+        randlet = alphabet[Math.floor(Math.random()*alphabet.length)];
+    }
+
+    guesses.push(tries);
+
+    document.getElementById('guesscount').innerHTML = guesscount;
+    document.getElementById('guesses').innerHTML = guesses;
+
+}
